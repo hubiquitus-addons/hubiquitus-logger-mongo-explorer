a9ba05e3a2fb3500e2fb0d746bf43ee96e92a39b
etissieres <etienne.tissieres@gmail.com>
2014-01-09 12:12:27 +0100

    enable some grunt tasks

--- webapp/Gruntfile.js
+++ webapp/Gruntfile.js
@@ -390,10 +390,10 @@ module.exports = function (grunt) {
     'concat',
     'ngmin',
     'copy:dist',
-    //'cdnify',
+    'cdnify',
     'cssmin',
     'uglify',
-    //'rev',
+    'rev',
     'usemin',
     'htmlmin'
   ]);
