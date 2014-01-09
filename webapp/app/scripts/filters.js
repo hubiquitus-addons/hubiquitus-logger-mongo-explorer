cbb3d88e4fbbe3b7254c9c5c3b674fb82cfdd30d
etissieres <etienne.tissieres@gmail.com>
2014-01-09 11:32:03 +0100

    use native json filter

--- webapp/app/scripts/filters.js
+++ webapp/app/scripts/filters.js
@@ -19,12 +19,6 @@ angular.module('webapp').filter('bootstraplvl', function () {
   };
 });
 
-angular.module('webapp').filter('prettyjson', function () {
-  return function (input) {
-    return JSON.stringify(input, null, 2);
-  };
-});
-
 angular.module('webapp').filter('prettydate', function () {
   return function (input) {
     return moment(input).format('YYYY-MM-DD hh:mm:ss Z');
